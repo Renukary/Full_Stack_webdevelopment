a47e5021d4662db59d98ce388f019906e6ee7845
Renukary <renukayalehole@gmail.com>
2023-11-18 20:44:33 +0530

    Login and Register
    Front end page for Banking and payments which has login and register page and stores data into database.

---
+++ pycrud/db_config.py
@@ -0,0 +1,9 @@
+from app import app
+from flaskext.mysql import MySQL
+mysql = MySQL()
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Renuka@#2023'
+app.config['MYSQL_DATABASE_DB'] = 'bankingcrud'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
---
+++ pycrud/webcontroller.py
@@ -0,0 +1,118 @@
+import pymysql
+from app import app
+from db_config import mysql
+from flask import render_template, redirect
+from flask import flash, request
+
+@app.route('/', methods=['GET', 'POST'])
+def start():
+    return render_template('home.html', title='HOME - Landing Page')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
+
+@app.route('/submit', methods=['POST'])
+def login_submit():
+    _username = request.form['inputUsername']
+    _password = request.form['inputPassword']
+    if _username and _password and request.method == 'POST':
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        sql = "SELECT * FROM employee WHERE username=%s"
+        sql_where = (_username,)
+        cursor.execute(sql, sql_where)
+    else:
+        flash('Invalid username/password!')
+        return redirect('/login')
+
+
+@app.route('/', methods=['GET'])
+def add_employee():
+   return render_template("view.html",datas=fetchListOfEmployees())
+
+@app.route("/add", methods=["POST"])
+def home():
+ try:
+   _id=request.form.get('id')
+   _name=request.form.get('uname')
+   _email=request.form.get('email')
+   _salary=request.form.get('salary')
+   sql = "INSERT INTO employee(id, uname, email,salary) VALUES(%s, %s, %s ,  %s)"
+   data = (_id, _name, _email,_salary)
+   conn = mysql.connect()
+   cursor = conn.cursor()
+   cursor.execute(sql, data)
+   conn.commit()
+   return render_template('view.html',datas=fetchListOfEmployees())
+ except Exception as e:
+   print(e)
+ finally:
+   cursor.close()
+   conn.close()
+
+@app.route('/deleteEmployee', methods=['POST'])
+def deleteEmployee():
+ try:
+   _id=request.form.get('id')
+   print(_id)
+   conn = mysql.connect()
+   cursor = conn.cursor()
+   cursor.execute("DELETE FROM employee WHERE id=%s", (_id))
+   conn.commit()
+   return render_template('view.html',datas=fetchListOfEmployees())
+ except Exception as e:
+  print(e)
+ finally:
+  cursor.close()
+  conn.close()
+def fetchListOfEmployees():
+ try:
+   conn = mysql.connect()
+   cursor = conn.cursor(pymysql.cursors.DictCursor)
+   cursor.execute("SELECT * FROM employee")
+   rows = cursor.fetchall()
+   return rows
+ except Exception as e:
+   print(e)
+ finally:
+   cursor.close()
+   conn.close()
+
+@app.route('/employeeUpdate', methods=['POST'])
+def update_employee():
+ try:
+   _id=request.form.get('id')
+   _name=request.form.get('uname')
+   _email=request.form.get('email')
+   _salary=request.form.get('salary')
+
+   sql = "UPDATE employee SET uname=%s, email=%s, salary=%s WHERE id=%s"
+   data = (_name, _email, _salary, _id,)
+   conn = mysql.connect()
+   cursor = conn.cursor()
+   cursor.execute(sql, data)
+   conn.commit()
+   return render_template('view.html',datas=fetchListOfEmployees())
+ except Exception as e:
+  print(e)
+ finally:
+  cursor.close()
+  conn.close()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='8080')
